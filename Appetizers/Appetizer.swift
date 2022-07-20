3c85a629dcb363532e5d85e5a093664ff70b0822
Sadman Adib <sadmanadib67@gmail.com>
2022-07-20 11:01:04 +0600

    Appetizer Model and json mock data added

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,32 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Sadman Adib on 20/7/22.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request : [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 001, name: "Test Appetizer", description: "This is description", price: 10.50, imageURL: "", calories: 99, protein: 99, carbs: 99)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+    
+}
+
+
