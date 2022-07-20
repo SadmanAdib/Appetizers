50d2d997a5970d6daf4c14e22c2d63964e4a81cc
Sadman Adib <sadmanadib67@gmail.com>
2022-07-20 10:04:01 +0600

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Sadman Adib on 20/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
