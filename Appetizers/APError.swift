bccd4b471a901e63658ce1c83d5fb87563f4c2da
Sadman Adib <sadmanadib67@gmail.com>
2022-07-20 12:35:58 +0600

    Network Manager and Error added

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Sadman Adib on 20/7/22.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
