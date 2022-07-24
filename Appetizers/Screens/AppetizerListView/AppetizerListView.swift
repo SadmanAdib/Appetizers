6c84acc1b67193b13c7ebfd1a0e65cb5b72903e2
Sadman Adib <sadmanadib67@gmail.com>
2022-07-24 15:52:45 +0600

    Data passed to DetailView successfully

--- Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct AppetizerListView: View {
     
     @StateObject var viewModel = AppetizerListViewModel()
-    @State var isShowingDetailView = false
+    @State private var isShowingDetailView = false
+    @State private var selectedAppetizer: Appetizer?
     
     var body: some View {
         ZStack{
@@ -18,17 +19,20 @@ struct AppetizerListView: View {
                 List(viewModel.appetizers){ appetizer in
                    AppetizerListCell(appetizer: appetizer)
                         .onTapGesture {
+                            selectedAppetizer = appetizer
                             isShowingDetailView = true
                         }
                 }
                 .navigationTitle("Appetizers")
+                .disabled(isShowingDetailView)
             }
             .onAppear{
                 viewModel.getAppetizers()
             }
+            .blur(radius: isShowingDetailView ? 20 : 0)
             
             if isShowingDetailView {
-                AppetizerDetailView(appetizer: MockData.sampleAppetizer, isShowingDetailView: $isShowingDetailView)
+                AppetizerDetailView(appetizer: selectedAppetizer!, isShowingDetailView: $isShowingDetailView)
             }
             
             if viewModel.isLoading {
@@ -38,9 +42,7 @@ struct AppetizerListView: View {
         .alert(item: $viewModel.alertItem) { alertItem in
             Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
         }
-        
     }
-    
 }
 
 struct AppetizerListView_Previews: PreviewProvider {
